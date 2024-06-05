2048e9315201bb7cb9b767e3f3ba6c2588c010eb
gumingchen <1240235512@qq.com>
2024-06-05 23:47:14 +0800

    update

--- src/views/layout/components/sidebar/components/tabbar/components/tab-friend/index.vue
+++ src/views/layout/components/sidebar/components/tabbar/components/tab-friend/index.vue
@@ -1,12 +1,11 @@
 <template>
-  <Tab label="好友" value="friend" @click="router.push({ name: 'friend' })">
+  <Tab label="好友" value="friend">
     <EpUser></EpUser>
   </Tab>
 </template>
 
 <script setup>
 import Tab from '../tab/index.vue'
-const router = useRouter()
 </script>
 
 <style lang="scss" scoped>
