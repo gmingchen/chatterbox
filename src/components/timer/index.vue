3e6815b00362b05a943c1e072977ee8430688da4
gumingchen <1240235512@qq.com>
2024-06-20 13:26:38 +0800

    update

--- src/components/timer/index.vue
+++ src/components/timer/index.vue
@@ -10,13 +10,14 @@ const time = ref('00:00')
 
 const init = () => {
   clearInterval(timer)
-  time.value = '00:00'
+  time.value = '00:00:00'
 }
 
 const start = () => {
   const start = dayjs()
   timer = setInterval(() => {
-    time.value = dayjs(dayjs().diff(start)).format('mm:ss')
+    // 超过一天 时长会不对哦 应该也不会出现超过一天的吧
+    time.value = dayjs(dayjs().diff(start)).subtract(8, 'hour').format('HH:mm:ss')
   }, 1000)
 }
 
